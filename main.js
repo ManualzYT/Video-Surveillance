ec942d73042cc8f89d04828d7b648262bda8e6cf
ManualzYT <75396652+ManualzYT@users.noreply.github.com>
2021-10-16 22:19:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+video = "";
+status = "";
+objects = [];
+
+function preload(){
+   video = createVideo('video.mp4');
+   video.hide();
+}
+
+function setup(){
+
+    canvas = createCanvas(800,600);
+    canvas.center();
+}
+
+function draw(){
+    image(video,0,0,800,600);
+    if(status !=""){
+        objectDetector.detect(video,gotResult);
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "status: object detected";
+            document.getElementById("number_of_objects").innerHTML = "number of objects detected are:" + objects.length;
+            fill("black");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x + 15 , objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+
+        }
+    }
+    
+
+}
+
+function gotResult(error,results){
+  if(error){
+      console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status: detecting objects";
+}
+
+function modelLoaded(){
+    console.log("cocossd model is laoded");
+    status = true;
+    video.loop();   
+    video.speed(1);
+    video.volume(0);                    
+}
